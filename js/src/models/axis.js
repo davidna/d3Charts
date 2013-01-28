a1fae902282991e90aa5311c3f9d5a7f5621b3cc
David Na <dna@davidna.com>
2013-01-27 16:12:54 -0800

    axis-constructor accepts options-JSON; dimensions option bound to property

--- js/src/models/axis.js
+++ js/src/models/axis.js
@@ -1,6 +1,10 @@
 define([ ], function() {
-	var d3ChartAxisModel = function() {
+	var d3ChartAxisModel = function(options) {
+		console.log('options: ', options);
 
+		if (options) {
+			this.dimensions = options.dimensions;
+		}
 	};
 
 	return d3ChartAxisModel;
--- tests/unit/unitAxisModel.js
+++ tests/unit/unitAxisModel.js
@@ -2,17 +2,17 @@ define([
 
 	'../../js/src/chart'
     , '../../js/src/models/axis'
-    , '../../js/src/models/legend'
-    , '../../js/src/models/bar'
-    , '../../js/src/models/barChart'
+    // , '../../js/src/models/legend'
+    // , '../../js/src/models/bar'
+    // , '../../js/src/models/barChart'
 
 ], function (
 
     D3Chart
     , Axis
-    , Legend
-    , Bar
-    , BarChart
+    // , Legend
+    // , Bar
+    // , BarChart
 
 ) {
 
@@ -24,7 +24,33 @@ define([
             });
 
             describe('--- d3Chart.models.axis ---', function() {
-                
+                var axis;
+
+                beforeEach(function() {
+                    axis = new Axis();
+                });
+
+                it('should accept a JSON of options', function() {
+                    var optionsJSON = {
+                        dimensions: 2
+                        , orientation: 'horizontal'
+                        , start: 0
+                        , end: 100
+                        , unit: 'px'
+                    };
+
+                    expect(function() { axis = new Axis(optionsJSON); }).not.toThrow(new Error());
+                });
+
+                it('given optionsJSON with [dimensions:2], should fill its property [dimensions] with value [2]', function() {
+                    var optionsJSON = {
+                        dimensions: 2
+                    };
+
+                    axis = new Axis(optionsJSON);
+
+                    expect(axis.dimensions).toBe(2);
+                });
             });
         });
 
