f748614a0637143bc09ab167a6681a7f427f8bc3
David Na <dna@davidna.com>
2013-01-29 13:55:57 -0800

    render accepts JSON with width, height, margins key

--- js/src/chart.js
+++ js/src/chart.js
@@ -12,37 +12,55 @@ define([], function() {
 
 		this.setInWindow = function() {
 			window.d3Chart = this;
-		};
+		}
 
 		this.destroy = function() {
 			window.d3Chart = undefined;
-		};
+		}
 
 		this.addModel = function (model) {
 			//console.log(typeof model);
 			//console.log(model);
 
 			this.models.push(model);
-		};
+		}
 
 		this.render = function(options) {
 			if (options) {
 				console.log('options: ', options);
 
-				var selectedArray = d3.selectAll(options.selector);
+				if (options.width) this.setWidth(options.width);
+				if (options.height) this.setHeight(options.height);
+				if (options.margins) this.setMargins(options.margins);
 
-				//console.log('selected: ', selectedArray ? selectedArray.length : 'empty');
+				if (options.selector) {
+					var selectedArray = d3.selectAll(options.selector);
 
-				if (selectedArray) {
-					var selectedItem = selectedArray[options.itemIndex];
-					var selectedModel = this.models[options.modelIndex];
+					//console.log('selected: ', selectedArray ? selectedArray.length : 'empty');
 
-					console.log('selectedItem: ', selectedItem);
-					console.log('selectedModel: ', selectedModel);	
+					if (selectedArray) {
+						var selectedItem = selectedArray[options.itemIndex];
+						var selectedModel = this.models[options.modelIndex];
 
-					selectedItem.innerHTML = selectedModel.toString();
+						console.log('selectedItem: ', selectedItem);
+						console.log('selectedModel: ', selectedModel);	
+
+						selectedItem.innerHTML = selectedModel.toString();
+					}
 				}
 			}
+		}
+
+		this.setWidth = function(width) {
+			console.log('setWidth: ', width);
+		}
+
+		this.setHeight = function(height) {
+			console.log('setHeight: ', height);
+		}
+
+		this.setMargins = function(marginsJSON) {
+			console.log('setMargin: ', marginsJSON);
 		}
 		
 		// this.graphs = []; // generated graphs (charts) on screen
